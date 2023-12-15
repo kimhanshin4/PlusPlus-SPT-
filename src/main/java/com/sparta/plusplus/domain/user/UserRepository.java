281adfd80ea06d2383eee38822f8080a6b642a72
kimhanshin4 <funold1004@gmail.com>
2023-12-15 17:05:42 +0900

    refactoring : 정리

--- src/main/java/com/sparta/plusplus/domain/user/UserRepository.java
+++ src/main/java/com/sparta/plusplus/domain/user/UserRepository.java
@@ -11,6 +11,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByNickname(String nickname);
 
-    boolean existsByNickname(String inputNickname);
+    boolean existsByNickname(String nickname);
 
 }
--- src/main/java/com/sparta/plusplus/global/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/plusplus/global/config/WebSecurityConfig.java
@@ -63,8 +63,8 @@ public class WebSecurityConfig {
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
                 .permitAll() // resources 접근 허용 설정
                 .requestMatchers("/").permitAll() // 메인 페이지 요청 허가
-                .requestMatchers("/api/users/**").permitAll() // '/api/users/'로 시작하는 요청 모두 접근 허가
                 .requestMatchers("/api/posts").permitAll() // '/api/post 까지 접근 허가
+                .requestMatchers("/api/users/**").permitAll() // '/api/users/'로 시작하는 요청 모두 접근 허가
                 .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
 
